55690e56011fd732a2ec92a61110094eed47e00c
princevenkat <gvenkat1988@gmail.com>
2025-01-13 14:21:07 +0530

    Updates

---
+++ precache-manifest.cced4e177b9831914f739b33aa933a3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2381f3886348145ac6d18b095392e8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da959e783603c751a00c",
+    "url": "/static/css/main.44c6f668.chunk.css"
+  },
+  {
+    "revision": "18a4336b0a2c28ee86de",
+    "url": "/static/js/2.300729a6.chunk.js"
+  },
+  {
+    "revision": "da959e783603c751a00c",
+    "url": "/static/js/main.2c69bfb0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
